0622f8ca28cece778a3ba0ca7ebba66666e78bb2
Brishti Shil <brishtishil62@gmail.com>
2022-05-28 09:29:58 +0600

    update data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,13 +16,35 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('manufacturer').collection('services');
+        // const parchaseCollection = client.db('manufacturer').collection('parchase');
 
-        app.get('/service', async (req, res) => {
+        app.get('/get-service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.post('/add-service', async (req, res) => {
+            const data = req.body;
+            const result = await serviceCollection.insertOne(data); res.send(result);
+        });
+
+        app.put('/update-service/:id', async (req, res) => {
+            const { id } = req.params;
+            const data = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = { $set: data };
+            const option = { upsert: true };
+            const result = await serviceCollection.updateOne(filter, updateDoc, option);
+            res.send(result);
         })
+
+        // app.post('/parchase', async (req, res) => {
+        //     const parchase = req.body;
+        //     const result = await parchaseCollection.insertOne(parchase);
+        //     res.send(result);
+        // })
 
     }
     finally {
